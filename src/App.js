88a643e661388d5efdcfc5543034f79c689f2951
Christian Zintel <c.zintel@yahoo.de>
2021-06-05 21:28:08 +0200

    claim authorship

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
 
 function App() {
   return (
@@ -19,7 +18,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
