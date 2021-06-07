966c8309a9eb2fd2a39549337d9c15be56c0b7fc
Christian Zintel <c.zintel@yahoo.de>
2021-06-07 17:45:44 +0200

    add global style

---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+* {
+    box-sizing: border-box
+  }
+ 
+  body {
+    margin: 0;
+    font-family: sans-serif;
+    font-size: 112.5%;
+    line-height: 1.4;
+  }
+`
