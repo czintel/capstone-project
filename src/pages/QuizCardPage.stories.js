7a7c4a31da8189348a798a0fcdc87ffc71b26b38
Christian Zintel <c.zintel@yahoo.de>
2021-06-11 14:06:47 +0200

    added QuizCardPage storybook file

---
+++ src/pages/QuizCardPage.stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import QuizCardPage from './QuizCardPage'
+
+export default {
+  title: 'Pages/QuizCardPage',
+  component: QuizCardPage,
+}
+
+const Template = args => <QuizCardPage {...args} />
+
+export const DefaultLearningCard = Template.bind({})
+DefaultLearningCard.args = {
+  title: 'Stell dir vor...',
+  scenario:
+    'Du spielst gerade mit deinen Freunden ein Fußballmatch und bist kurz davor, ein Tor zu schießen. In diesem Moment foult dich ein Gegner, um dich an deinem Tor zu hindern und lacht dich anschließend aus.',
+  question: 'Welches Gefühl spürst du in diesem Moment?',
+  answers: ['Freude', 'Angst', 'Wut', 'Kummer', 'Ekel'],
+}
