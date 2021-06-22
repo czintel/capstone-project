c12b4fe8d11302c950b83c891af3e16720ad873a
Christian Zintel <c.zintel@yahoo.de>
2021-06-22 17:17:11 +0200

    add basic form asking for username

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,14 +1,32 @@
 import styled from 'styled-components/macro'
+import PropTypes from 'prop-types'
 
-export default function StartPage() {
+StartPage.propTypes = {
+  onSubmit: PropTypes.func,
+}
+
+export default function StartPage(onSubmit) {
   return (
     <PageWrapper>
-      <h2>Willkommen zu AppName!</h2>
-      <span>
-        Klicke auf "Lernen", um mehr über die Basisemotionen zu erfahren.
-      </span>
+      <h2>Willkommen zu Hmpf!</h2>
+      <span>Schön, dass du da bist 😇</span>
+      <form>
+        <label onSubmit={handleSubmit}>
+          Wie ist dein Name?
+          <input name="name" type="text" placeholder="Dein Name" />
+        </label>
+      </form>
     </PageWrapper>
   )
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const input = form.elements.name
+    onSubmit(input.value)
+    form.reset()
+    input.focus()
+  }
 }
 
 const PageWrapper = styled.section`
