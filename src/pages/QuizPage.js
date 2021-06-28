bcdc919191d0fa4a40b8bc47153f111d0d1257d1
Christian Zintel <c.zintel@yahoo.de>
2021-06-28 22:26:15 +0200

    added nav bar icons. additional css updates

--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -37,11 +37,11 @@ export default function QuizPage() {
 const PageWrapper = styled.section`
   display: flex;
   flex-wrap: wrap;
-  padding: 20px 0;
   justify-content: center;
   gap: 20px;
   width: 100%;
   margin: 0 auto;
+  margin-top: 20px;
 `
 const FilterButton = styled.img`
   height: 45px;
