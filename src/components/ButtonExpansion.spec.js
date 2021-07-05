604d30c167dfd54e0a392432c5a5a1afcf44e969
Christian Zintel <c.zintel@yahoo.de>
2021-07-05 11:45:07 +0200

    added test for button expansion

--- src/components/ButtonExpansion.spec.js
+++ src/components/ButtonExpansion.spec.js
@@ -1,11 +1,38 @@
 import { render, screen } from '@testing-library/react'
-import ButtonExpansion from './ButtonExpansion'
+import userEvent from '@testing-library/user-event'
+import Button from './Button'
 
-describe('Button', () => {
+describe('ButtonExpanded', () => {
   it('contains a text', () => {
-    render(<ButtonExpansion onClick={() => {}}>Click me</ButtonExpansion>)
+    render(<Button onClick={() => {}}>Klick mich</Button>)
 
-    const button = screen.getByRole('button', { name: 'Click me' })
+    const button = screen.getByRole('button', { name: 'Klick mich' })
     expect(button).toBeInTheDocument()
+  })
+
+  it('calls onClick', () => {
+    const handleClick = jest.fn()
+    render(<Button onClick={handleClick}>Klick mich</Button>)
+
+    const button = screen.getByRole('button', { name: 'Klick mich' })
+    userEvent.click(button)
+
+    expect(handleClick).toHaveBeenCalled()
+  })
+
+  it('changes with the prop isExpanded', () => {
+    const { rerender } = render(<Button onClick={jest.fn()}>Klick mich</Button>)
+
+    const button = screen.getByRole('button')
+
+    const defaultStyle = getComputedStyle(button)
+
+    rerender(
+      <Button isExpanded onClick={jest.fn()}>
+        Klick mich
+      </Button>
+    )
+    const expandedStyle = getComputedStyle(button)
+    expect(expandedStyle).not.toBe(defaultStyle)
   })
 })
